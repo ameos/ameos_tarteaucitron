62a30451b3886bf0482069c70b9b2f05ccc883b9
Luc MULLER <l.muller@ameos.local>
2022-04-06 10:21:04 +0200

    [FEATURE]Add support of ameos_dailymotion to convert dailymotion video to TAC placeholder [FEATURE]Add ameos_dailymotion as suggested extension [FEATURE]Raise version to 1.2.16

---
+++ Classes/Xclasses/Ameos/AmeosDailymotion/Rendering/DailymotionRenderer.php
@@ -0,0 +1,131 @@
+<?php
+namespace Ameos\AmeosTarteaucitron\Xclasses\Ameos\AmeosDailymotion\Rendering;
+
+use TYPO3\CMS\Core\Resource\File;
+use TYPO3\CMS\Core\Resource\FileInterface;
+use TYPO3\CMS\Core\Resource\FileReference;
+use TYPO3\CMS\Core\Resource\OnlineMedia\Helpers\OnlineMediaHelperInterface;
+use TYPO3\CMS\Core\Resource\OnlineMedia\Helpers\OnlineMediaHelperRegistry;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+class DailymotionRenderer implements \TYPO3\CMS\Core\Resource\Rendering\FileRendererInterface
+{
+    /**
+     * Returns the priority of the renderer
+     * This way it is possible to define/overrule a renderer
+     * for a specific file type/context.
+     *
+     * For example create a video renderer for a certain storage/driver type.
+     *
+     * Should be between 1 and 100, 100 is more important than 1
+     *
+     * @return int
+     */
+    public function getPriority()
+    {        
+        return 1;
+    }
+
+    /**
+     * Check if given File(Reference) can be rendered
+     *
+     * @param FileInterface $file File or FileReference to render
+     * @return bool
+     */
+    public function canRender(FileInterface $file)
+    {
+        return ($file->getMimeType() === 'video/dailymotion' || $file->getExtension() === 'dailymotion') && $this->getOnlineMediaHelper($file) !== false;
+    }
+    
+    /**
+     * Get online media helper
+     *
+     * @param FileInterface $file
+     * @return bool|OnlineMediaHelperInterface
+     */
+    protected function getOnlineMediaHelper(FileInterface $file)
+    {
+        if ($this->onlineMediaHelper === null) {
+            $orgFile = $file;
+            if ($orgFile instanceof FileReference) {
+                $orgFile = $orgFile->getOriginalFile();
+            }
+            if ($orgFile instanceof File) {
+                $this->onlineMediaHelper = OnlineMediaHelperRegistry::getInstance()->getOnlineMediaHelper($orgFile);
+            } else {
+                $this->onlineMediaHelper = false;
+            }
+        }
+        return $this->onlineMediaHelper;
+    }
+    
+    /**
+     * Render for given File(Reference) HTML output
+     *
+     * @param FileInterface $file
+     * @param int|string $width TYPO3 known format; examples: 220, 200m or 200c
+     * @param int|string $height TYPO3 known format; examples: 220, 200m or 200c
+     * @param array $options
+     * @param bool $usedPathsRelativeToCurrentScript See $file->getPublicUrl()
+     * @return string
+     */
+    public function render(FileInterface $file, $width, $height, array $options = [], $usedPathsRelativeToCurrentScript = false)
+    {
+        if (!isset($options['autoplay']) && $file instanceof FileReference) {
+            $autoplay = $file->getProperty('autoplay');
+            if ($autoplay !== null) {
+                $options['autoplay'] = $autoplay;
+            }
+        }
+
+        $urlParams = [];
+        if (!empty($options['autoplay'])) {
+            $urlParams[] = 'autoPlay=1';
+        }
+        
+        if ($file instanceof FileReference) {
+            $orgFile = $file->getOriginalFile();
+        } else {
+            $orgFile = $file;
+        }
+
+        $videoId = $this->getOnlineMediaHelper($file)->getOnlineMediaId($orgFile);
+
+        $attributes = ['allowfullscreen'];
+        if ((int)$width > 0) {
+            $attributes[] = 'width="' . (int)$width . '"';
+        }
+        if ((int)$height > 0) {
+            $attributes[] = 'height="' . (int)$height . '"';
+        }
+        if (is_object($GLOBALS['TSFE']) && $GLOBALS['TSFE']->config['config']['doctype'] !== 'html5') {
+            $attributes[] = 'frameborder="0"';
+        }
+        foreach (['class', 'dir', 'id', 'lang', 'style', 'title', 'accesskey', 'tabindex', 'onclick', 'poster', 'preload'] as $key) {
+            if (!empty($options[$key])) {
+                $attributes[] = $key . '="' . htmlspecialchars($options[$key]) . '"';
+            }
+        }
+
+        return sprintf(
+
+            '<div class="dailymotion_player" videoID="%s"%s></div>',
+            $videoId,
+            empty($attributes) ? '' : ' ' . implode(' ', $attributes)
+        );
+    }
+    
+}
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,41 +1,48 @@
 <?php
 
 /***************************************************************
- * Extension Manager/Repository config file for ext "Ameos.AmeosTarteaucitron".
+ * Extension Manager/Repository config file for ext "ameos_tarteaucitron".
  *
-  *
+ * Auto generated 06-04-2022 09:33
+ *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = [
-	'title'              => 'AMEOS - TarteAuCitron (GDPR cookie banner and tracking management / French RGPD compatible)',
-	'description'        => 'Integrates tarteaucitron.js in TYPO3 (see : https://tarteaucitron.io )',
-	'category'           => 'frontend',
-	'shy'                => 0,
-	'version'            => '1.2.15',
-	'conflicts'          => '',
-	'priority'           => '',
-	'loadOrder'          => '',
-	'module'             => '',
-	'state'              => 'stable',
-	'uploadfolder'       => 0,
-	'createDirs'         => '',
-	'modify_tables'      => '',
-	'clearCacheOnLoad'   => 1,
-	'lockType'           => '',
-	'author'             => 'Ameos Team',
-	'author_email'       => 'typo3dev@ameos.com',
-	'author_company'     => 'Ameos',
-	'CGLcompliance'      => '',
-	'CGLcompliance_note' => '',
-	'constraints' => [
-		'conflicts' => [],
-		'suggests'  => [],
-		'depends'   => [
-			'typo3'            => '8.6.0-11.99.99',
-		],
-	],
-    'autoload' => ['psr-4' => ['Ameos\\AmeosTarteaucitron\\' => 'Classes']],	
-];
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'AMEOS - TarteAuCitron (GDPR cookie banner and tracking management / French RGPD compatible)',
+  'description' => 'Integrates tarteaucitron.js in TYPO3 (see : https://tarteaucitron.io )',
+  'category' => 'frontend',
+  'version' => '1.2.16',
+  'state' => 'stable',
+  'uploadfolder' => false,
+  'createDirs' => '',
+  'clearCacheOnLoad' => 1,
+  'author' => 'Ameos Team',
+  'author_email' => 'typo3dev@ameos.com',
+  'author_company' => 'Ameos',
+  'constraints' => 
+  array (
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => array(
+      'ameos_dailymotion' => '1.0.0-1.0.99',
+    ),
+    array (
+    ),
+    'depends' => array (
+      'typo3' => '8.6.0-11.99.99',
+    ),
+  ),
+  'autoload' => 
+  array (
+    'psr-4' => 
+    array (
+      'Ameos\\AmeosTarteaucitron\\' => 'Classes',
+    ),
+  ),
+  'clearcacheonload' => true,
+);
+
